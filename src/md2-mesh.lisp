f795d123c7b4fbedc326c70b0951320a35ded57e
cage <cage@katamail.com>
2018-07-01 20:56:18 +0200

    - fixing again paths that meets a closed door.

--- src/md2-mesh.lisp
+++ src/md2-mesh.lisp
@@ -1765,11 +1765,11 @@ to take care of that"
   (declare (ignore dt))
   (with-common-calc-move (entity end)
     (if (and (game-state:invalicable-in-next-path-tile-p state entity current-path 1)
-             (game-state:door-in-next-path-tile-p        state current-path        1))
+             (not (game-state:door-in-next-path-tile-p        state current-path        1)))
         (%stop-movement entity :decrement-movement-points t :interrupt-plan-if-ai t)
         ;; open door if any and if closed
         (let ((door-opened-p (manage-door-ai state entity current-path 1)))
-          (when (not door-opened-p) ;; door just opened the current plan has been interrupted
+          (when (not door-opened-p) ;; door was open, the plan has not been interrupted
             (if (gconf:config-smooth-movements)
                 (setf (pos entity)
                       (vec+ (pos entity) (vec* dir +model-move-speed+)))
